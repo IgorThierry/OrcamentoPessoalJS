4871b4056e9d695653b939ab6773f99ddc24346e
Igor Thierry <igorthierry15@gmail.com>
2019-09-22 12:07:53 -0300

    incluido botão deletar

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ class Bd {
                 continue
             }
 
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -93,6 +94,10 @@ class Bd {
         }
 
         return despesasFiltrada;
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -178,6 +183,18 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar botão de exclusão
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_','')
+            bd.remover(id)
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
     })
 }
 
